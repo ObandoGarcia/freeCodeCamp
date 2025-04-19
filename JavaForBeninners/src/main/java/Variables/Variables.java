b2bae0e4b32a264c11df8f25f99816326308b04f
Jose Manuel Obando <57765554+ObandoGarcia@users.noreply.github.com>
2025-04-19 11:22:14 -0600

    Eliminacion de datos antiguos
    Eliminarcion de datos antiguos

--- JavaForBeninners/src/main/java/Variables/Variables.java
+++
@@ -1,9 +0,0 @@
-package Variables;
-
-public class Variables {
-    public static void main(String[] args) {
-        int age = 27;
-
-        System.out.println("I am " + age + " years old.");
-    }
-}
